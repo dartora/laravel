e44288492338bcfcf9e79d7cd695e1ee3ae8fa1c
Dartora <rodolfo.darto.da.silva@gmail.com>
2023-08-31 12:47:39 -0300

    almost there

--- app/Http/Controllers/PusherController.php
+++ app/Http/Controllers/PusherController.php
@@ -7,19 +7,20 @@ use Illuminate\Http\Request;
 
 class PusherController extends Controller
 {
-    //
     public function index()
     {
         return view('index');
     }
-    public function broadcast($request)
+
+    public function broadcast(Request $request)
     {
         broadcast(new PusherBroadcast($request->get('message')))->toOthers();
-        return view('broadcast', ['message', $request->get('message')]);
-    }
-    public function receive($request)
-    {
 
+        return view('broadcast', ['message' => $request->get('message')]);
+    }
+
+    public function receive(Request $request)
+    {
         return view('receive', ['message' => $request->get('message')]);
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,37 +2,92 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Chat Laravel Pusher | Edlin App</title>
+    <link rel="icon" href="https://assets.edlin.app/favicon/favicon.ico" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- JavaScript -->
     <script src="https://js.pusher.com/7.2/pusher.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+    <!-- End JavaScript -->
+
+    <!-- CSS -->
     <link rel="stylesheet" href="/style.css">
+    <!-- End CSS -->
 
 </head>
 
 <body>
     <div class="chat">
 
-
+        <!-- Header -->
         <div class="top">
             <div>
-                <img src="https://media.licdn.com/dms/image/C4D03AQFBjQlxWjLAKA/profile-displayphoto-shrink_800_800/0/1520375796380?e=1698883200&v=beta&t=MSvsnlIidIQWft4OcDjTPXi3ozABB0b5NtlyEfKMpVk" alt="" class="src">
-                <p>Matheus</p>
-                <small>online</small>
-
+                <p>Ross Edlin</p>
+                <small>Online</small>
             </div>
         </div>
+        <!-- End Header -->
+
+        <!-- Chat -->
         <div class="messages">
-            @include('receive',['message' => 'Hello World'])
+            @include('receive', ['message' => "Hey! What's up!  👋"])
+            @include('receive', ['message' => "Ask a friend to open this link and you can chat with them!"])
+            @include('broadcast', ['message' =>"Ask a friend to open this link and you can chat with them!"])
+
         </div>
+        <!-- End Chat -->
+
+        <!-- Footer -->
         <div class="bottom">
             <form>
-                <input type="text" id="message" name="message" placeholder="Type a message...">
+                <input type="text" id="message" name="message" placeholder="Enter message..." autocomplete="off">
                 <button type="submit"></button>
             </form>
         </div>
+        <!-- End Footer -->
+
     </div>
 </body>
+
+<script>
+    const pusher = new Pusher("{{config('broadcasting.connections.pusher.key')}}", {
+        cluster: 'eu'
+    });
+    const channel = pusher.subscribe('public');
+
+    //Receive messages
+    channel.bind('chat', function(data) {
+        $.post("/receive", {
+                _token: '{{csrf_token()}}',
+                message: data.message,
+            })
+            .done(function(res) {
+                $(".messages > .message").last().after(res);
+                $(document).scrollTop($(document).height());
+            });
+    });
+
+    //Broadcast messages
+    $("form").submit(function(event) {
+        event.preventDefault();
+
+        $.ajax({
+            url: "/broadcast",
+            method: 'POST',
+            headers: {
+                'X-Socket-Id': pusher.connection.socket_id
+            },
+            data: {
+                _token: '{{csrf_token()}}',
+                message: $("form #message").val(),
+            }
+        }).done(function(res) {
+            $(".messages > .message").last().after(res);
+            $("form #message").val('');
+            $(document).scrollTop($(document).height());
+        });
+    });
+</script>
 
 </html>
